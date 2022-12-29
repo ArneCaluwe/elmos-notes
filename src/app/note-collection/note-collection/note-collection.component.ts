55fab18c03dce947b0b09239dac552c1cccec9fb
Arne Caluwe <arne.caluwe@outlook.com>
2022-12-29 18:51:37 +0100

    use databinding to supply note data

--- src/app/note-collection/note-collection/note-collection.component.ts
+++ src/app/note-collection/note-collection/note-collection.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
+import { Note } from 'src/app/models/note.model';
 
 @Component({
   selector: 'app-note-collection',
   templateUrl: './note-collection.component.html',
-  styleUrls: ['./note-collection.component.scss']
+  styleUrls: ['./note-collection.component.scss'],
 })
 export class NoteCollectionComponent {
-
+  note1: Note = {
+    noteTitle: 'presents to buy',
+    noteContent: 'wall-decoration',
+  };
+  note2: Note = {
+    noteTitle: 'my wishlist',
+    noteContent: 'running shoes',
+  };
+  note3: Note = {
+    noteTitle: 'people to ignore',
+    noteContent: 'John legend',
+  };
 }
--- src/app/note/note.component.ts
+++ src/app/note/note.component.ts
@@ -1,13 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Note } from '../models/note.model';
 
 @Component({
   selector: 'app-note',
   standalone: true,
   imports: [CommonModule],
   templateUrl: './note.component.html',
-  styleUrls: ['./note.component.scss']
+  styleUrls: ['./note.component.scss'],
 })
 export class NoteComponent {
-
+  @Input()
+  note?: Note;
 }
